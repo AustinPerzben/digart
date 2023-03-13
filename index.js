2ae1f946d39e11978a9039c45bc798e2b7ec6ef9
austin <aguspdc@gmail.com>
2023-03-13 08:26:01 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// === GLOBALS === //
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBhWygDUviicrrudYLmnSrXQ8Z4sh4DZD4",
+  authDomain: "capstone-space.firebaseapp.com",
+  projectId: "capstone-space",
+  storageBucket: "capstone-space.appspot.com",
+  messagingSenderId: "930500695794",
+  appId: "1:930500695794:web:3ca9c01512c1385a3ce26b",
+  measurementId: "G-TKCFZJHVCB",
+};
+// // Get a reference to the database service
+// const auth = getAuth(app);
+// // All the regions, channels, and memes
+
+// onAuthStateChanged(auth, (user) => {
+//   if (user != null) {
+//     console.log("User is signed in");
+//   } else {
+//     console.log("User is not signed in");
+//   }
+// });
+export let app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+
+export function saveData() {
+  console.log("Saving data to database...");
+  let txt_field = document.getElementById("name");
+  if (txt_field.value == "") {
+    txt_field.placeholder = "Please enter a name!";
+    return;
+  } else if (txt_field.value == "admin") {
+    loadData();
+    return;
+  }
+
+  const reference = ref(db, "tutorial/" + txt_field.value);
+  set(reference, {
+    path: drawing[0],
+    color: palette[0]
+  });
+  console.log("Wrote data to database");
+}
+
+export function loadData() {
+  const reference = ref(db, "tutorial/");
+  onValue(reference, (snapshot) => {
+    let data = snapshot.val();
+    console.log("Reading database...");
+    visualizeData(data);
+  });
+}
---
+++ sketch.js
@@ -0,0 +1,139 @@
+let permissionGranted = false
+let isAdmin = false
+let drawing = [];
+let palette = [];
+let currentPath = [];
+let isCapturing = false;
+let drawingStart;
+
+let _x, _y, _z
+let mn = 0
+let mx = 0
+
+// will handle first time visiting to grant access
+function onAskButtonClicked() {
+  DeviceMotionEvent.requestPermission().then(response => {
+    if (response === 'granted') {
+      permissionGranted = true
+    } else {
+      permissionGranted = false
+    }
+    this.remove()
+  }).catch(console.error)
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  super_btn = createButton("Start Dancing!");
+  super_btn.position(width / 2 - 50, height - 100);
+  super_btn.style('margin', '0 auto');
+  super_btn.size(100);
+  super_btn.mousePressed(startPath);
+  translate(width / 2, height / 2);
+
+  if (typeof (DeviceMotionEvent.requestPermission) === 'function') {
+    DeviceOrientationEvent.requestPermission()
+      .catch(() => {
+        // show permission dialog only the first time
+        // it needs to be a user gesture (requirement) in this case, click
+        let askButton = createButton("Allow acess to sensors")
+        askButton.style("font-size", "24px")
+        askButton.position(0, 0)
+        askButton.mousePressed(onAskButtonClicked)
+        throw error // keep the promise chain as rejected
+      })
+      .then(() => {
+        // this runs on subsequent visits
+        permissionGranted = true
+      })
+  } else {
+    // it's up to you how to handle non ios 13 devices
+    permissionGranted = true
+  }
+}
+
+function startPath() {
+  drawingStart = millis();
+  isCapturing = true;
+  currentPath = [];
+  drawing.push(currentPath);
+  palette.push(select('#picker').value());
+  super_btn.hide();
+}
+
+function endPath() {
+  drawingStart = null;
+  isCapturing = false;
+}
+
+function visualizeData(data) {
+  isAdmin = true;
+  drawing = [];
+  palette = [];
+  let keys = Object.keys(data);
+  let values = Object.values(data);
+  for (let i = 0; i < keys.length; i++) {
+    drawing.push(values[i].path);
+    palette.push(values[i].color);
+  }
+  console.log(drawing, palette);
+}
+
+function draw() {
+  background('#333');
+  // line(width / 2, 0, width / 2, height);
+  // mx = max(mx,accelerationZ)
+  // mn = min(mn, accelerationZ)
+  // text(floor(mn)+","+ceil(mx),width/2-10, 50)
+  if (!permissionGranted) {
+    return
+  }
+  if (isCapturing && !isAdmin) {
+    _x = mouseX;
+    _y = mouseY;
+    _z = 10;
+    let state = {
+      x: _x,
+      y: _y,
+      sz: _z
+    };
+    currentPath.push(state);
+    if (drawingStart && millis() - drawingStart > 10000) {
+      endPath();
+    }
+  }
+
+
+  strokeWeight(4);
+  noFill();
+  for (let i = 0; i < drawing.length; i++) {
+    let path = drawing[i];
+    let col = palette[i];
+    stroke(col);
+    beginShape();
+    for (let j = 0; j < path.length; j++) {
+      vertex(path[j].x, path[j].y);
+    }
+    endShape();
+  }
+}
+
+function keyPressed() {
+  if (key == " ") {
+    isAdmin = false;
+    drawing = [];
+    super_btn.show();
+  } else if (key == "Escape") {
+    endPath();
+  }
+}
+
+function touchStarted() {
+  if (touches.length == 2) {
+    isAdmin = false;
+    drawing = [];
+    super_btn.show();
+  } else if (touches.length == 1) {
+    endPath();
+  }
+}
